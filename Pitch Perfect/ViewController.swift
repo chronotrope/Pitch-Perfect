073d5dde140af6900234950374e5115ad7817c5d
Ruben Robles <ruben@Air-Ruben.home>
2015-02-19 23:55:38 -0800

    Lesson 3A - started using Navigation Controller

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingInProgress: UILabel!
+    
+    @IBOutlet weak var recordButton: UIButton!
+    
+    @IBOutlet weak var stopButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func viewWillAppear(animated: Bool) {
+        //Hide the stop button
+        stopButton.hidden = true
+    }
+    @IBAction func recordAudio(sender: UIButton) {
+        
+        
+        //TODO: Record the user's voice
+        println("in recordAudio")
+        recordingInProgress.hidden = false
+        stopButton.hidden = false
+        recordButton.enabled = false
+    }
+   
+    @IBAction func stopButtonPressed(sender: UIButton) {
+        println("stop button pressed...")
+        recordingInProgress.hidden = true
+    }
 }
 
