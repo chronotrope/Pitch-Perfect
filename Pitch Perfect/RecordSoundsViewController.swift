2685066bad7aaef19041c343501f877905451b54
Ruben Robles <ruben@Air-Ruben.home>
2015-03-12 23:11:58 -0700

    Had to restart lesson 1, “Intro to IOS App Development in Swift" Started @ 2.8 today Ended @ 2.13

--- Pitch Perfect/RecordSoundsViewController.swift
+++ Pitch Perfect/RecordSoundsViewController.swift
@@ -12,13 +12,17 @@ import AVFoundation
 
 class RecordSoundsViewController: UIViewController {
 
+  
+   
     @IBOutlet weak var recordingInProgress: UILabel!
-    @IBOutlet weak var recordButton: UIButton!
-    @IBOutlet weak var stopButton: UIButton!
+    
+    
+   
+    
     
     //Declared Globally
     
-    var audioRecorder:AVAudioRecorder!
+    //var audioRecorder:AVAudioRecorder!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,44 +37,23 @@ class RecordSoundsViewController: UIViewController {
 
     override func viewWillAppear(animated: Bool) {
         //Hide the stop button
-        stopButton.hidden = true
+        //stopButton.hidden = true
     }
+    
+    
     @IBAction func recordAudio(sender: UIButton) {
-        
-        println("in recordAudio")
+        //TODO:  Show text "recording in progress"
         recordingInProgress.hidden = false
-        stopButton.hidden = false
-        recordButton.enabled = false
         
-        //TODO: Record the user's voice
+        //TODO:  Record the user's voice
+        println("in recordAudio")
+       
         
-        let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
-        
-        let currentDateTime = NSDate()
-        let formatter = NSDateFormatter()
-        formatter.dateFormat = "ddMMyyyy-HHmmss"
-        let recordingName = formatter.stringFromDate(currentDateTime)+".wav"
-        let pathArray = [dirPath, recordingName]
-        let filePath = NSURL.fileURLWithPathComponents(pathArray)
-        println(filePath)
-        
-        var session = AVAudioSession.sharedInstance()
-        session.setCategory(AVAudioSessionCategoryPlayAndRecord, error: nil)
-        
-        audioRecorder = AVAudioRecorder(URL: filePath, settings: nil, error: nil)
-        audioRecorder.meteringEnabled = true
-        audioRecorder.prepareToRecord()
-        audioRecorder.record()
         
         
     }
    
-    @IBAction func stopButtonPressed(sender: UIButton) {
-        println("stop button pressed...")
-        recordingInProgress.hidden = true
-        audioRecorder.stop()
-        var audioSession = AVAudioSession.sharedInstance()
-        audioSession.setActive(false, error: nil)
-    }
+    
+   
 }
 
