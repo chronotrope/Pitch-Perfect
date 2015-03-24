857d1265cd63f82b3832563e85a5ed0fea331f3a
Ruben Robles <ruben@Air-Ruben.home>
2015-03-24 00:12:18 -0700

    Moved PlaySoundsViewController.swift to main project folder...successful attempt

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -2,7 +2,7 @@
 //  PlaySoundsViewController.swift
 //  Pitch Perfect
 //
-//  Created by Ruben Robles on 2/24/15.
+//  Created by Ruben Robles on 3/23/15.
 //  Copyright (c) 2015 Integration Dojo. All rights reserved.
 //
 
@@ -11,18 +11,12 @@ import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
     var audioPlayer: AVAudioPlayer!
-    
-    
-    
-    
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // Do any additional setup after loading the view.
         
-        println("Finished playing the song")
         
         if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3")
         {
@@ -30,37 +24,33 @@ class PlaySoundsViewController: UIViewController {
             audioPlayer = AVAudioPlayer(contentsOfURL: filePathUrl, error: nil)
             audioPlayer.enableRate = true
             
-            }
+        }
         else {
             println("The file path is empty.")
         }
-        
-        
-        
-        
-        
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    
+
     /*
     // MARK: - Navigation
-    
+
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-    // Get the new view controller using segue.destinationViewController.
-    // Pass the selected object to the new view controller.
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
     }
     */
     
+
     @IBAction func playSlowAudio(sender: UIButton) {
         // Play audio sloooowly here...
         
-
+        
         // code from Udacity.com below
         //good practice to stop audio player before you start it
         audioPlayer.stop()
@@ -68,24 +58,24 @@ class PlaySoundsViewController: UIViewController {
         audioPlayer.currentTime = 0.0
         audioPlayer.play()
         println("Finished playing slow audio")
-        
-        
     }
     
     @IBAction func playFastAudio(sender: UIButton) {
+        // Play fast audio here...
         
+        
+        // code from Udacity.com below
+        //good practice to stop audio player before you start it
         audioPlayer.stop()
         audioPlayer.rate = 2.0
         audioPlayer.currentTime = 0.0
         audioPlayer.play()
-        println("Finished playing fast audio")
-
+        println("Finished playing slow audio")
+        
     }
-    
+
     @IBAction func stopAudioPlayer(sender: UIButton) {
         audioPlayer.stop()
         println("I stopped audio player")
     }
-    
-    
 }
